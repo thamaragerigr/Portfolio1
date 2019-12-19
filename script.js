35d1b90b3b90903f0418c9d677563eb385081d71
Thamara Gerig <thamaragerigr@gmail.com>
2019-12-19 12:11:35 +0100

    Changes generales

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+  $(function() {
+    $('.chart').easyPieChart({
+      scaleColor: false,
+      lineWidth: 10,
+      lineCap: 'round',
+      barColor: '#333',
+      size: 150,
+      animate: 500
+    });
+  });
+
+
+  // When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
